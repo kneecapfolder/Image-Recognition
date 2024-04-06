4885aac3af2e82dd8f15cf93c37b9d73b8469173
kneecapFolder <ilan7gr@gmail.com>
2024-04-06 11:34:45 +0300

    mainly a gui redesign

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const webcam = document.getElementById('webcam');
 var mediaDevices = navigator.mediaDevices;
 var on = false;
 var model = undefined;
-var children = [];
 var bounding = webcam.getClientRects().item(0);
 
 function toggleCam() {
@@ -32,13 +31,18 @@ function toggleCam() {
 }
 
 cocoSsd.load().then(laodedModel => model = laodedModel);
+
 function predictWebcam() {
+    if (!on) {
+        clearFrames();
+        return;
+    }
+
     // Start classifying a frame in the stream
     model.detect(webcam).then(predictions => {
         // Remove all detection frames
-        for(let i = 0; i < children.length; i++)
-            container.removeChild(children[i]);
-        children.splice(0);
+        clearFrames();
+        
 
         // Draw the confident predictions
         for(let i = 0; i < predictions.length; i++) {
@@ -46,6 +50,7 @@ function predictWebcam() {
                 const p = document.createElement('p');
                 p.innerText = `${predictions[i].class} - ${Math.round(parseFloat(predictions[i].score*100))}%`;
 
+                // Create a highlight frame
                 const highlighter = document.createElement('div');
                 highlighter.classList.add('highlighter');
                 highlighter.style.left = `${bounding.left + scale(predictions[i].bbox[0], 1280, parseInt(webcam.width))}px`;
@@ -55,13 +60,17 @@ function predictWebcam() {
 
                 highlighter.appendChild(p);
                 container.appendChild(highlighter);
-                children.push(highlighter);
             }
         }
 
         // Call function when browser is ready
         window.requestAnimationFrame(predictWebcam);
     });
+}
+
+function clearFrames() {
+    for(let i = 1; i < container.children.length; i++)
+        container.removeChild(container.children[i]);
 }
 
 function scale(n, origin, scale) {
